1654a73cbc8e8bb58040695c9fa462555882ca11
faldiic <aldi44375@gmail.com>
2025-06-03 17:20:25 +0700

    Test Live API

--- front-end/src/components/UploadPhoto.jsx
+++ front-end/src/components/UploadPhoto.jsx
@@ -113,7 +113,7 @@ const UploadPhoto = () => {
       // Kirim ke backend Flask
       const formData = new FormData();
       formData.append('file', file);
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://dragf-ai-test-production.up.railway.app/predict', {
         method: 'POST',
         body: formData,
       });
